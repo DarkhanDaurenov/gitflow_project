4ee7c1bc1901525d96b78f403fdabf1dec34186c
DarkhanDaurenov <147601220+DarkhanDaurenov@users.noreply.github.com>
2023-12-26 16:56:47 +0600

    str_func.py with Docstring

--- str_func.py
+++ str_func.py
@@ -1,5 +1,6 @@
 # Функция принимает строку и возвращает ее с заглавными буквами
 def letter_upper(letter):
+    """Функция возвращает строку в верхнем регистре"""
     return letter.upper()
 
 print(letter_upper('skypro'))
