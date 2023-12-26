2ad4768e6e1edffdba35a7e3c90195e84679b1ca
DarkhanDaurenov <takishevdarkhan@gmail.com>
2023-12-26 16:53:40 +0600

    commit str_func.py with Docstring

--- str_func.py
+++ str_func.py
@@ -1,5 +1,16 @@
 # Функция принимает строку и возвращает ее с заглавными буквами
 def letter_upper(letter):
+    """Функция возвращает строку в верхнем регистре"""
     return letter.upper()
 
+
 print(letter_upper('skypro'))
+
+
+#Функция принимает строку и возаращает с заглавными буквами
+def letter_title(letter):
+    """Функция возвращает строку с заглавными буквами"""
+    return letter.title()
+
+
+print(letter_title('skypro one of the best'))
